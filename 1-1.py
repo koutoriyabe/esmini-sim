e3483fe30e7470371bc1b90b26359fc755c583e3
kodama-tier4 <143679523+kodama-tier4@users.noreply.github.com>
2023-08-31 16:41:15 +0900

    Add files via upload

---
+++ 1-1.py
@@ -0,0 +1,12 @@
+import xml.etree.ElementTree as ET
+
+v0 = 10  # Egoの初速　単位はm/s
+vp = 50  # 歩行者の速度　単位はm/s
+Sx_p = 0  # 歩行者の
+Dy = 10
+B2y = Dy - 2
+
+
+# XMLファイルを読み込む
+tree = ET.parse("/home/hideakikodama/esmini/resources/tier4/1-1.xosc")
+root = tree.getroot()
